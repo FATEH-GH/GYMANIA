fffd4785ae3cd17a61c7c3e9ebfaf55dd8093596
FATEH-GH <fatehghribi27@gmail.com>
2024-02-13 21:25:49 +0100

    env update

--- app/api/exercise/[id]/route.ts
+++ app/api/exercise/[id]/route.ts
@@ -8,7 +8,7 @@ export const GET = async (
   const response = await fetch(url, {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": process.env.API_KEY!,
+      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_API_KEY!,
       "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
     },
   });
--- app/api/target/route.ts
+++ app/api/target/route.ts
@@ -6,7 +6,7 @@ export const GET = async (_: NextRequest) => {
   const response = await fetch(url, {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": process.env.API_KEY!,
+      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_API_KEY!,
       "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
     },
   });
